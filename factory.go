954f6889ea5cab46b1354d42702ddb4038f42145
Mateo León Camacho <mlc.d@posteo.la>
2022-11-01 14:33:46 -0500

    Added brief description for factory.go

--- factory.go
+++ factory.go
@@ -1,3 +1,9 @@
+/*
+The factory pattern allow us to use a single method to create different types, as long
+as those types implement the same methods as the factory interface; in this example,
+we have two different types of PCs (laptops and desktops), and each one of them wraps
+a 'Computer' type.
+*/
 package main
 
 import (
@@ -74,9 +80,14 @@ func printNameAndStock(p IProduct) {
 }
 
 func main() {
-	laptop, _ := GetComputerFactory("laptop")
-	desktop, _ := GetComputerFactory("desktop")
+	const (
+		laptop  = "laptop"
+		desktop = "desktop"
+	)
+	// we can tell there won't be any errors in this example, so we can discard those returns
+	laptopPC, _ := GetComputerFactory(laptop)
+	desktopPC, _ := GetComputerFactory(desktop)
 
-	printNameAndStock(laptop)
-	printNameAndStock(desktop)
+	printNameAndStock(laptopPC)
+	printNameAndStock(desktopPC)
 }
